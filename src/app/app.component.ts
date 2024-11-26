cd60a6e9e417adbe1013706a6b5958d0d9d2da87
Henrique Serafin <henriqueserafin8@gmail.com>
2024-11-25 21:54:57 -0300

    Primeira versao do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ClientComponent } from "./client/client.component";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+import { MasterComponent } from "./master/master.component";
+import { ReportsComponent } from "./reports/reports.component";
+import { SignupComponent } from "./signup/signup.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatSlideToggleModule,
+    ClientComponent,
+    HomeComponent,
+    LoginComponent,
+    MasterComponent,
+    ReportsComponent,
+    SignupComponent
+],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'advocacia-final';

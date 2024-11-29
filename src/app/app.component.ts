b9ee2e2a400c9bd73e66a70ff3c45c7e3e1cda47
Anderson Mares <anderson.psn07@gmail.com>
2024-11-28 21:26:48 -0300

    Subindo código

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { ClientComponent } from "./client/client.component";
-import { HomeComponent } from "./home/home.component";
-import { LoginComponent } from "./login/login.component";
-import { MasterComponent } from "./master/master.component";
-import { ReportsComponent } from "./reports/reports.component";
-import { SignupComponent } from "./signup/signup.component";
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    MatSlideToggleModule,
-    ClientComponent,
-    HomeComponent,
-    LoginComponent,
-    MasterComponent,
-    ReportsComponent,
-    SignupComponent
+    NavbarComponent,
+    FooterComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './_pages/home/home.component';
+import { ContatoComponent } from './components/contato/contato.component';
+import { LoginComponent } from './_pages/login/login.component';
+import { MeetingComponent } from './_pages/meeting/meeting.component';
+import { DashboardComponent } from './_pages/dashboard/dashboard.component';
+import { SignupComponent } from './_pages/signup/signup.component';
+import { ClientComponent } from './_pages/client/client.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'contato', component: ContatoComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'meeting', component: MeetingComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'cliente', component: ClientComponent },
+];
